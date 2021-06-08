12758bc6d00d3708e6cf0eb2f92db79668852e92
Bohdan Kostiuk <kostiuk2002@gmail.com>
2021-06-08 21:10:54 +0300

    Added one test case

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <Polygon.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    Polygon ConvexHull;
+
+
+    Polygon P({{0, 3}, {2, 2}, {1, 1}, {2, 1},
+               {3, 0}, {0, 0}, {3, 3}});
+
+
+    ConvexHull = P.GetConvexHullJarvisAlgorithm();
+
+
+    //Print
+    std::copy(ConvexHull.begin(),
+              ConvexHull.end(),
+              std::ostream_iterator<Point>(std::cout, "\n"));
+
     return 0;
 }

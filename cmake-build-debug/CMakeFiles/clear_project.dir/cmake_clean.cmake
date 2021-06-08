f19a774ad892fa251a1768466756b70167d79b4e
Bohdan Kostiuk <kostiuk2002@gmail.com>
2021-06-08 17:08:58 +0300

    Setup of repo's start point

---
+++ cmake-build-debug/CMakeFiles/clear_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clear_project.dir/main.cpp.o"
+  "clear_project.exe"
+  "clear_project.pdb"
+  "libclear_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clear_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
